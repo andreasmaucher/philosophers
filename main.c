25892629b0aa9424090be4f3eae00254983895a0
Andreas Maucher <andreas-maucher@gmx.de>
2023-07-20 12:09:48 +0200

    first local clone

---
+++ main.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amaucher <amaucher@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 11:40:01 by amaucher          #+#    #+#             */
+/*   Updated: 2023/07/20 11:40:03 by amaucher         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+/* 
+number_of_philos (same number as forks)
+time_to_die
+time_to_eat
+time_to_sleep
+number of times each philo must eat (optional)
+*/
