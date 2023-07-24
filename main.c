510329e654de59c55c8a0ce74332fd8a6357a638
Andreas Maucher <andreas-maucher@gmx.de>
2023-07-24 12:38:03 +0200

    added multiple functions and started the beginning of the routine

--- main.c
+++ main.c
@@ -94,32 +94,152 @@ int create_philos(t_data *data)
     return (0);
 }
 
-//! change to ft_atoi
-t_data *initialize_data_struct(int ac, char **av)
+void	*monitor(void *data_pointer)
 {
-	t_data	*data;
+	t_philo	*philo;
 
-	data = malloc(sizeof(t_data));
-	if (data == NULL)
-		return (NULL);
+	philo = (t_philo *) data_pointer;
+	pthread_mutex_lock(&philo->data->write);
+	printf("data val: %d", philo->data->dead);
+	pthread_mutex_unlock(&philo->data->write);
+	while (philo->data->dead == 0)
+	{
+		pthread_mutex_lock(&philo->lock);
+		if (philo->data->finished >= philo->data->n_philos)
+			philo->data->dead = 1;
+		pthread_mutex_unlock(&philo->lock);
+	}
+	return ((void *)0);
+}
+
+
+
+/* initializing & creating threads; if n_meals parameter is set monitor
+thread is created o check when all philos have eaten n_meals*/
+int	thread_init(t_data *data)
+{
+	int			i;
+	pthread_t	t0;
+
+	i = -1;
+	data->start_time = get_time();
+	if (data->n_meals > 0)
+	{
+		if (pthread_create(&t0, NULL, &monitor, &data->philo[0]))
+			return (error(TH_ERR, data));
+	}
+	while (++i < data->n_philos)
+	{
+		if (pthread_create(&data->id[i], NULL, &routine, &data->philo[i]))
+			return (error(TH_ERR, data));
+		ft_usleep(1);
+	}
+	i = -1;
+	while (++i < data->n_philos)
+	{
+		if (pthread_join(data->id[i], NULL))
+			return (error(JOIN_ERR, data));
+	}
+	return (0);
+}
+
+int	init_philo_struct(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->n_philos)
+	{
+		data->philo[i].data = data;
+		data->philo[i].id = i + 1;
+		data->philo[i].time_to_die2 = data->time_to_die;
+		data->philo[i].n_eat_times = 0;
+		data->philo[i].eating = 0;
+		data->philo[i].status = 0;
+		pthread_mutex_init(&data->philo[i].lock, NULL);
+		i++;
+	}
+	return (0);
+}
+
+/*sets the left fork of the first philosopher (data->philo[0]) 
+to point to the first fork in the data->forks array;
+sets the right fork of the first philosopher to point to the 
+last fork in the data->forks array. This establishes a circular 
+arrangement since the last fork is adjacent to the first fork
+each philosopher is connected to its left and right neighbor, 
+and the last philosopher is connected back to the first one
+ex: philo 1 left fork is the right fork of philo 2*/
+int	init_forks(t_data *data)
+{
+	int	i;
+
+	i = -1;
+	while (++i < data->n_philos)
+		pthread_mutex_init(&data->forks[i], NULL);
+	i = 0;
+	data->philo[0].fork_l = &data->forks[0];
+	data->philo[0].fork_r = &data->forks[data->n_philos - 1];
+	i = 1;
+	while (i < data->n_philos)
+	{
+		data->philo[i].fork_l = &data->forks[i];
+		data->philo[i].fork_r = &data->forks[i - 1];
+		i++;
+	}
+	return (0);
+}
+
+//! change to ft_atoi
+int init_data_struct(t_data *data, int ac, char **av)
+{
+	data->id = malloc(sizeof(pthread_t) * data->n_philos);
+	if (!data->id)
+		return (1);
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->n_philos);
+	if (!data->forks)
+		return (1);
+	data->philo = malloc(sizeof(t_philo) * data->n_philos);
+	if (!data->philo)
+		return (1);
 	data->n_philos = atoi(av[1]);
 	data->time_to_die = atoi(av[2]);
 	data->time_to_eat = atoi(av[3]);
 	data->time_to_sleep = atoi(av[4]);
 	if (ac == 6)
-		data->n_eat_times = atoi(av[5]);
+		data->n_meals = atoi(av[5]);
+	else
+		data->n_meals = -1;
 	data->start_time = get_time();
-	create_philos(data);
+	data->dead = 0;
+	data->finished = 0;
+	pthread_mutex_init(&data->write, NULL);
+	pthread_mutex_init(&data->lock, NULL);
+	/* create_philos(data);
 	create_forks(data);
-	join_threads(data);
-	return (data);
+	join_threads(data); */
+	return (0);
+}
+
+int init_structs(t_data *data, int ac, char **av)
+{
+	if (init_data_struct(data, ac, av))
+		return (1);
+	if (init_forks(data))
+		return (1);
+	init_philo_struct(data);
+	return(0);
 }
 
 int	main(int ac, char *av[])
 {
-	if (ac == 6)
-	{
-		initialize_data_struct(ac, av);
-	}
+	t_data data;
+
+	if (ac < 5 || ac > 6)
+		return (1);
+	if (init_structs(&data, ac, av))
+		return (1);
+	if (init_threads(&data))
+		return (1);
 	return 0;
 }
--- philo.h
+++ philo.h
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef data_H
-# define data_H
+#ifndef PHILO_H
+# define PHILO_H
 
 # include <stdio.h>
 # include <stdlib.h>
@@ -21,6 +21,20 @@
 # include <stdint.h> // uint64_t
 # include <unistd.h> // usleep
 
+//	alloc_err
+# define ALLOC_ERR_1 "ERROR WHILE ALLOCATING THREADS IDs"
+# define ALLOC_ERR_3 "ERROR WHILE ALLOCATING PHILOS"
+# define ALLOC_ERR_2 "ERROR WHILE ALLOCATING FORKS"
+//	input_err
+# define ERR_IN_1 "INVALID INPUT CHARACTER"
+# define ERR_IN_2 "INVALID INPUT VALUES"
+//	pthread_err
+# define TH_ERR "ERROR WHILE CREATING THREADS"
+# define JOIN_ERR "ERROR WHILE JOINING THREADS"
+# define INIT_ERR_1 "ERROR WHILE INIT FORKS"
+//	time_err
+# define TIME_ERR "UNABLE TO RETRIVE UTC"
+
 typedef enum e_data_state
 {
 	EATING = 0,
@@ -29,12 +43,20 @@ typedef enum e_data_state
 	DEAD = 3,
 }	t_state;
 
+struct	s_data;
+
 typedef struct s_philo
 {
+	struct s_data	*data;
+	int				id;
 	int			philo_id;
 	int			n_eat_times;
-	uint64_t	time_to_die;
+	uint64_t	time_to_die2;
+	pthread_t		t1;
+	int				status;
+	int				eating;
 	t_state		state;
+	pthread_mutex_t	lock;
 	pthread_mutex_t		*fork_l;
 	pthread_mutex_t		*fork_r;
 
@@ -42,15 +64,20 @@ typedef struct s_philo
 
 typedef struct s_data
 {
-	int			thread_id;
+	t_philo		*philo;
+	pthread_t	*id;
 	pthread_t	*philo_threads;
 	int			n_philos;
+	int			n_meals;
+	int			dead;
+	int			finished;
 	uint64_t	time_to_die;
 	uint64_t	time_to_eat;
 	uint64_t	time_to_sleep;
-	int			n_eat_times;
 	uint64_t	start_time;
-	pthread_mutex_t	*forks;	
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	lock;
+	pthread_mutex_t	write;
 
 }	t_data;
 
